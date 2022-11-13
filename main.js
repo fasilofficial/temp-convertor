4e215084f492e7b86038089c5f3b55b0b75981f4
fasilofficial <muhammedfasilofficial@gmail.com>
2022-11-13 11:10:10 +0530

    First commit

---
+++ main.js
@@ -0,0 +1,12 @@
+var celsius = document.getElementById('celsius');
+var fahrenheit = document.getElementById('fahrenheit');
+
+function celToFahr() {
+    let output = ( parseFloat(celsius.value) * 9/5 ) + 32;
+    fahrenheit.value = parseFloat(output.toFixed(2));
+}
+
+function fahrToCel() {
+    let output = ( parseFloat(fahrenheit.value) - 32) * 5/9;
+    celsius.value = parseFloat( output.toFixed(2));
+}
